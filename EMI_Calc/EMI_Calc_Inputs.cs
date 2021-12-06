e53e23c1b744997d150a1d55b6eacd248af177f5
golu-10 <gauri.dangui@kilowott.com>
2021-12-06 11:53:35 +0530

    commit

---
+++ EMI_Calc/EMI_Calc_Inputs.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMI_Calc
+{
+    internal class EMI_Calc_Inputs
+    {
+        int  n,t;
+        int PrincipalAmt;
+        double RateOfInterest;
+            
+        public void GetPrincipal (int p )
+        {
+            PrincipalAmt = p;
+            
+        }
+
+        public void GetRate(double  r)
+        {
+            RateOfInterest = r;
+            
+        }
+
+        public void GetDuration (int m)
+        {
+            t = m;
+        }
+
+        public void GetMonthlyCompound ( int x)
+        {
+            n = x;
+        }
+
+        public void EmiCalculation ()
+        {
+            decimal emi;
+            emi = 0;
+            emi = (decimal)(PrincipalAmt * Math.Pow((1 + RateOfInterest / n), (t * n)));
+             emi = emi / 24;
+            Console.WriteLine("The calculated EMI is : " + Math.Round(emi, 2));
+        }
+
+
+
+
+    }
+}
---
+++ EMI_Calc/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EMI_Calc
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int t, n, PrincipalAmt;
+            double interest;
+            Console.WriteLine("Enter the Principal Amount :");
+            PrincipalAmt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Rate of Interest :");
+            interest = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the loan duration :");
+            t = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the monthly compound :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            var emiObject = new EMI_Calc_Inputs();
+            emiObject.GetPrincipal(PrincipalAmt);
+            emiObject.GetRate(interest);
+            emiObject.GetMonthlyCompound(n);
+            emiObject.GetDuration(t);
+
+
+        }
+    }
+}
+
+
